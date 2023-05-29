0dc9e1b84205bf45fa51e21c8f8b6626e0538cd1
Linkinlog <dahlton@dahlton.org>
2023-05-28 23:46:54 -0400

    feat(pkgMan): Adds dnf, zypper, and pacman
    Adds structs that should work for those package managers

--- cmd/version.go
+++ cmd/version.go
@@ -11,6 +11,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Gasible.",
 	Long:  `All software has versions. This is ours.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Version: v0.1.0")
+		fmt.Println("Version: v0.1.1")
 	},
 }
